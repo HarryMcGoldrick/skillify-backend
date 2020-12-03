66de6419f1435d0b2d4b54dd9370d2c6d8dedd67
BuildTools <harry_mcgoldrick@msn.com>
2020-12-03 16:51:13 +0000

    add relevant comments

--- src/services/youtube.ts
+++ src/services/youtube.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// Returns a singular video matching the searchQuery
 export const youtubeRelatedSearch = async (searchQuery) => {
     return axios.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${searchQuery}&key=${process.env.YT_API_KEY}`).then(({data}) => {
         return data;
--- src/utils/authentication.ts
+++ src/utils/authentication.ts
@@ -1,11 +1,13 @@
 import { sign, verify } from 'jsonwebtoken';
 
+// Create a JWT token using the secret in the .env config
 export const generateJWT = (username) => {
     // Token expires in 6 hours
     const exp = Math.floor(Date.now() / 1000) + (60 * 60) * 6;
     return sign({username, exp }, process.env.TOKEN_SECRET);
 }
 
+// Check the request header and authenticate the token attached
 export const authenticateToken = (req, res, next) => {
     // Gather the jwt access token from the request header
     const authHeader = req.headers.authorization
